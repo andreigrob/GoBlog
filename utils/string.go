1f6f938e79f77f16e625025b980c4891069e65dd
andreigrob <154005278+andreigrob@users.noreply.github.com>
2025-06-06 10:25:37 -0400

    Fix comment typo

--- utils/string.go
+++ utils/string.go
@@ -1,6 +1,6 @@
 package utils
 
-// CountChars counts the number of occurences of a character in a string.
+// CountChars counts the number of occurrences of a character in a string.
 func CountChars(Str string, Ch rune) (n int) {
 	for _, ch := range Str {
 		if ch == Ch {
